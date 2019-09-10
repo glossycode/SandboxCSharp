3d03430b4b4a563db47484510cd7443d20ed5256
Olivier Champion <glossycode@me.com>
2019-09-10 16:30:41 +0200

    initial commit

---
+++ SandboxCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SandboxCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
