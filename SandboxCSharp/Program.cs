1348678738d9de7530adb8358bad31d2de49e6f7
Olivier Champion <glossycode@me.com>
2019-09-17 16:38:51 +0200

    added test selection

--- SandboxCSharp/Program.cs
+++ SandboxCSharp/Program.cs
@@ -7,13 +7,31 @@ namespace SandboxCSharp
     {
         static void Main(string[] args)
         {
+            int index = 1;
             foreach (var facto in UnityBuilder.Instance().Factories)
-            {                
-                Console.WriteLine($"\r\nRunning >> {facto.GetType().FullName}");
-                facto.Run(UnityBuilder.Instance().Container);
-                Console.WriteLine("");
+            {
+                Console.WriteLine($"{index++} - {facto.GetType().FullName}");
             }
-            
+            Console.WriteLine($"Choose the test to run (all : default or 0)");
+
+            int num = 0;
+            string selection = Console.ReadLine();
+            if (!string.IsNullOrEmpty(selection))
+            {
+                int.TryParse(selection, out num);
+            }
+
+            index = 1;
+            foreach (var facto in UnityBuilder.Instance().Factories)
+            {
+                if (num == index++ || num == 0)
+                {
+                    Console.WriteLine($"\r\nRunning >> {facto.GetType().FullName}");
+                    facto.Run(UnityBuilder.Instance().Container);
+                    Console.WriteLine("");
+                }
+            }
+
             Console.Write("\r\nHit a key to terminate ... ");
             Console.ReadKey();
         }
