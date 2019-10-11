d8cd39de4d315a360b7bb998e91c83d2295beeb2
Olivier Champion <glossycode@me.com>
2019-10-11 14:39:24 +0200

    Added Xml compare

---
+++ SandboxCSharp/Extention/XmlNodeExt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace SandboxCSharp.Extention
+{
+    public static class XmlNodeExt
+    {
+        
+        public static string FullString(this XmlNode value)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{value.Name}");
+            
+            for (int i= 0; i < value.Attributes.Count; i++)
+                sb.Append($" -{value.Attributes[i].Name}:{value.Attributes[i].Value}");
+            return sb.ToString();
+        }
+
+
+        public static string GetXPath(this XmlNode node)
+        {
+            StringBuilder builder = new StringBuilder();
+            while (node != null)
+            {
+                switch (node.NodeType)
+                {
+                    case XmlNodeType.Attribute:
+                        builder.Insert(0, "/@" + node.Name);
+                        node = ((XmlAttribute)node).OwnerElement;
+                        break;
+                    case XmlNodeType.Element:
+                        int index = FindElementIndex((XmlElement)node);
+                        builder.Insert(0, "/" + node.Name + "[" + index + "]");
+                        node = node.ParentNode;
+                        break;
+                    case XmlNodeType.Document:
+                        return builder.ToString();
+                    default:
+                        throw new ArgumentException("Only elements and attributes are supported");
+                }
+            }
+            throw new ArgumentException("Node was not in a document");
+        }
+        static int FindElementIndex(XmlElement element)
+        {
+            XmlNode parentNode = element.ParentNode;
+            if (parentNode is XmlDocument)
+            {
+                return 1;
+            }
+            XmlElement parent = (XmlElement)parentNode;
+            int index = 1;
+            foreach (XmlNode candidate in parent.ChildNodes)
+            {
+                if (candidate is XmlElement && candidate.Name == element.Name)
+                {
+                    if (candidate == element)
+                    {
+                        return index;
+                    }
+                    index++;
+                }
+            }
+            throw new ArgumentException("Couldn't find element within parent");
+        }
+    }
+}
---
+++ SandboxCSharp/XML/XMLCompare.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using Unity;
+using SandboxCSharp.Extention;
+
+namespace SandboxCSharp.XML
+{
+    class XMLCompare : IFactory
+    {
+        public void DoRegister(IUnityContainer Container)
+        {
+        }
+
+        public void Run(IUnityContainer Container)
+        {
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(@"XML\file1.xml");
+
+            XmlDocument doc2 = new XmlDocument();
+            doc2.Load(@"XML\file2.xml");
+
+            Compare(doc1, doc2);
+        }
+
+        private void Compare(XmlDocument doc1, XmlDocument doc2)
+        {
+            XmlNode root1 = doc1.DocumentElement;
+            XmlNode root2 = doc2.DocumentElement;
+
+            Console.WriteLine($"Compare doc1:\t{root1.FullString()}\r\nwith doc2:\t{root2.FullString()}");
+
+            NewMethod("new node in root1", root1, root2);
+            NewMethod("new node in root2", root2, root1);
+        }
+
+        private static void NewMethod(string comment, XmlNode reference, XmlNode nodeToCompare)
+        {
+            foreach (XmlNode child in reference.ChildNodes)
+            {
+                string xpath = child.GetXPath();
+                if (nodeToCompare.SelectSingleNode(xpath) == null)
+                {
+                    Console.WriteLine($"{comment} : {xpath}");
+                    Console.WriteLine($"    => content: {child.FullString()}");
+                    Console.WriteLine($"");
+                }
+            }
+        }
+    }
+}
